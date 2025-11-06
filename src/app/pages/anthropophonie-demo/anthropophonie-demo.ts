1c4c21a0bac5907c6b7b2ab55b92a49fd49f5a36
Damien Marill <dev@marill.dev>
2025-11-06 18:00:53 +0100

    feat: anthropophonie content

--- src/app/pages/anthropophonie-demo/anthropophonie-demo.ts
+++ src/app/pages/anthropophonie-demo/anthropophonie-demo.ts
@@ -1,13 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { HeroAnthropophonie } from '../../components/hero/variants/hero-anthropophonie.component';
 import { ExpertiseCard } from '../../components/expertise-card/expertise-card';
-import { ExpertiseService } from '../../services/expertise-service';
+import {ExpertiseKey, ExpertiseService} from '../../services/expertise-service';
+import {HeroLightcrowdComponent} from '../../components/hero/variants/hero-lightcrowd.component';
 
 @Component({
   selector: 'app-anthropophonie-demo',
   imports: [
     HeroAnthropophonie,
-    ExpertiseCard
+    ExpertiseCard,
+    HeroLightcrowdComponent
   ],
   templateUrl: './anthropophonie-demo.html',
   styleUrl: './anthropophonie-demo.css',
@@ -19,15 +21,19 @@ export class AnthrophonieDemo {
   protected readonly expertise = inject(ExpertiseService);
 
   // Collection des deliverables pour Anthropophonie dans l'ordre exact de la maquette
-  protected readonly anthropophonieDeliverables = this.expertise.getExpertiseCollection([
-    'background-research',
-    'benchmark',
-    'personas',
-    'user-case',
-    'composition-sketches',
-    'journey-map',
-    'user-flows',
-    'graphic-mockups',
-    'high-fidelity-prototype'
-  ]);
+  protected readonly anthropophonieDeliverables: {
+    id: ExpertiseKey
+    color: string
+  }[] = [
+    { id: "field-research", color: "rgba(179, 196, 94, 1)" },
+    { id: "shadowing", color: "rgba(121, 201, 188, 1)" },
+    { id: "data-collection", color: "rgba(237, 132, 175, 1)"},
+    { id: "personas", color: "rgba(225, 187, 10, 1)"},
+    { id: "user-case", color: "rgba(121, 201, 188, 1)"},
+    { id: "2x2-matrix", color: "rgba(255, 255, 255, 1)" },
+    { id: "journey-map", color: "rgba(202, 210, 39, 1)"},
+    { id: "facilitate-ideation-workshops", color: "#FFFFFF"},
+    { id: "low-fidelity-prototype", color: "rgba(237, 132, 175, 1)"},
+    { id: "concept-testing", color: "rgba(225, 187, 10, 1)"}
+  ];
 }
